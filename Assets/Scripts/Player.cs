ebb23f54c82aee89a4b9339803220a7a9a26c0ef
Massimosch <120001621+Massimosch@users.noreply.github.com>
2023-12-19 12:05:23 +0200

    Movement with new Input System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    void Start()
+    public float moveSpeed = 10.0f;
+    private Vector2 moveInput;
+    private PlayerControls controls;
+
+    private void Awake()
     {
-        
+        controls = new PlayerControls();
+
+        controls.Player.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Movement.canceled += ctx => moveInput = Vector2.zero;
+    }
+
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
     }
 
     void Update()
     {
-        
+    Vector3 movement = new Vector3(moveInput.x, moveInput.y, 0);
+    transform.position += movement * moveSpeed * Time.deltaTime;
     }
 }
