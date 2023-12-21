548967fb843633ce28773557e77e18d8c6e7e8ad
Massimosch <120001621+Massimosch@users.noreply.github.com>
2023-12-21 17:31:17 +0200

    Prefab updt
    Just updating prefabs and Dodgeball.cs

--- Assets/Scripts/Dodgeball.cs
+++ Assets/Scripts/Dodgeball.cs
@@ -6,6 +6,7 @@ public class Dodgeball : MonoBehaviour
 {
     public float throwForce = 10.0f;
     private Rigidbody2D rb;
+    private bool isHeld = false;
 
     private void Awake()
     {
@@ -16,11 +17,33 @@ public class Dodgeball : MonoBehaviour
     {
         transform.SetParent(playerTransform);
         rb.velocity = Vector2.zero;
+        isHeld = true;
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+{
+    Debug.Log("Dodgeball collided with: " + collision.gameObject.name);
+    // check if the dodgeball is not being held (has been thrown)
+    if (!IsHeld())
+    {
+        // check if the dodgeball has collided with an enemy after throw
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
 
     public void Throw(Vector2 direction)
     {
-    transform.SetParent(null);
-    rb.AddForce(Vector2.right * throwForce, ForceMode2D.Impulse);
+        transform.SetParent(null);
+        rb.AddForce(Vector2.right * throwForce, ForceMode2D.Impulse);
+        isHeld = false;
+    }
+
+    public bool IsHeld()
+    {
+        return isHeld;
     }
 }
